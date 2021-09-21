15633e868501e2bfc28774f4ad28442f69087385
Quang Vuong <quangv49@student.ubc.ca>
2021-09-20 20:25:07 -0700

    Added dates to the data fetching functions

--- data_gather.R
+++ data_gather.R
@@ -3,38 +3,56 @@ library(covidcast)
 #Gathering data from Covidcast
 
 bar_visit_prop <- covidcast_signal(data_source = "safegraph",
+                                   start_day = "2021-06-05",
+                                   end_day = "2021-09-05",
                                   signal = "bars_visit_prop",
                                   geo_type = "county", geo_values = "36061")
 
 restaurant_visit_prop <- covidcast_signal(data_source = "safegraph",
+                                          start_day = "2021-06-05",
+                                          end_day = "2021-09-05",
                                           signal = "restaurants_visit_prop",
                                           geo_type = "county", geo_values = "36061")
 
 mask_prop <- covidcast_signal(data_source = "fb-survey",
+                              start_day = "2021-06-05",
+                              end_day = "2021-09-05",
                               signal = "smoothed_wearing_mask_7d",
                               geo_type = "county", geo_values = "36061")
 
 other_mask_prop <- covidcast_signal(data_source = "fb-survey",
+                                    start_day = "2021-06-05",
+                                    end_day = "2021-09-05",
                               signal = "smoothed_wothers_masked_public",
                               geo_type = "county", geo_values = "36061")
 
 distancing <- covidcast_signal(data_source = "fb-survey",
+                               start_day = "2021-06-05",
+                               end_day = "2021-09-05",
                                 signal = "smoothed_wothers_distanced_public",
                                 geo_type = "county", geo_values = "36061")
 
 public_transit <- covidcast_signal(data_source = "fb-survey",
+                                   start_day = "2021-06-05",
+                                   end_day = "2021-09-05",
                                signal = "smoothed_wpublic_transit_1d",
                                geo_type = "county", geo_values = "36061")
 
 worked_outside <- covidcast_signal(data_source = "fb-survey",
+                                   start_day = "2021-06-05",
+                                   end_day = "2021-09-05",
                                 signal = "smoothed_wwork_outside_home_indoors_1d",
                                 geo_type = "county", geo_values = "36061")
 
 large_events <- covidcast_signal(data_source = "fb-survey",
+                                 start_day = "2021-06-05",
+                                 end_day = "2021-09-05",
                                  signal = "smoothed_wlarge_event_indoors_1d",
                                  geo_type = "county", geo_values = "36061")
 
 cases <- covidcast_signal(data_source = "indicator-combination",
+                          start_day = "2021-06-05",
+                          end_day = "2021-09-05",
                           signal = "confirmed_7dav_incidence_num",
                           geo_type = "county", geo_values = "36061")
 
